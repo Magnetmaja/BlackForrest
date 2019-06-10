7cfa7972f83193f6e374b037167cdea936ec8188
Magnetmaja <48548562+Magnetmaja@users.noreply.github.com>
2019-06-10 16:07:25 +0200

    Add files via upload

---
+++ Powershell/Create-LabOuStructure.ps1
@@ -0,0 +1,25 @@
+﻿
+function Create-LabOuStructure ($RootOUName) {
+ 
+$RootOuPath = (Get-ADDomain).DistinguishedName
+$RootOuLabName = $RootOUName
+
+$RootOU = New-ADOrganizationalUnit -Name $RootOuLabName -Path $RootOuPath -PassThru
+
+$ComputerOU = New-ADOrganizationalUnit -Name "Computers" -Path $RootOU.DistinguishedName -PassThru
+$UserOU = New-ADOrganizationalUnit -Name "Users" -Path $RootOU.DistinguishedName -PassThru
+$GroupOU = New-ADOrganizationalUnit -Name "Groups" -Path $RootOU.DistinguishedName -PassThru
+
+
+"Laptops", "Workstations", "Thin Client", "Tablets" |
+    % { New-ADOrganizationalUnit -Name $PSItem -Path $ComputerOU.DistinguishedName -PassThru }
+
+"Board", "Production", "Sales", "IT","Admins" |
+    % { New-ADOrganizationalUnit -Name $PSItem -Path $UserOU.DistinguishedName -PassThru }
+
+"Security Groups", "Resource Groups", "Administrative Groups", "Management Groups","Admin Group" |
+    % { New-ADOrganizationalUnit -Name $PSItem -Path $GroupOU.DistinguishedName -PassThru }
+
+    }
+
+    Create-LabOuStructure "Magnetmaja"
